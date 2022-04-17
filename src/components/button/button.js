c5e831e164d70164b401e6b60a0277dfe3226fef
MagdK <kovacsmagda@gmail.com>
2022-04-17 18:14:03 +0200

    Add Button function and if else to set up message change

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function Button() {
+    // https://www.robinwieruch.de/react-function-component/
+    const [message, setMessage] = useState('Click me!');
+        
+    const onClick = (e) => {
+        //console.log(`Clicked`)
+        if(message === 'Click me!') {
+            setMessage('You clicked me!');
+        } else if(message === 'You clicked me!') {
+            setMessage('Stop clicking me!')
+        }
+    };
+
     return (
         <div>
-            <button>Click me!</button>
+            <button onClick={onClick}>{message}</button>
         </div>
         
     );
