5f246a1831ef167a87cb8c98e9a1f40ea0ea5d57
MagdK <kovacsmagda@gmail.com>
2022-04-21 21:18:15 +0200

    show original message saved in a variant

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,6 +4,10 @@ function Button() {
     // https://www.robinwieruch.de/react-function-component/
     const [message, setMessage] = useState('Click me!');
         
+    const showOriginal = () => {
+        message.setMessage = 'Click me!'
+    }
+
     const onClick = (e) => {
         //console.log(`Clicked`)
         if(message === 'Click me!') {
@@ -12,6 +16,7 @@ function Button() {
             setMessage('Stop clicking me!')
         }
     };
+    // setTimeout(showPage, 3000);
 
     return (
         <div>
@@ -19,6 +24,8 @@ function Button() {
         </div>
         
     );
+
+    setTimeout(showOriginal, 3000);
 }
 
 export default Button;
